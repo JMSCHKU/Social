4fa4cc6df252b8272dec5cdf7eb2e90edc431f95
Cedric Sam <csam@lantau.jmsc.hku.hk>
2010-12-21 11:07:49 +0800

    Added support for search users

--- sinagetter.sh
+++ sinagetter.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 USAGE="usage: sinagetter.sh [option:1=user_timeline,2=users_show,3=friends,4=followers,5=tags] [user_id] [screen_name(for saving)] [page#] "
-SINAUSER="YOUR_USERNAME"
-SINAPASS="YOUR_PASSWORD"
-SINAAPPID="YOUR_APPID"
+SINAUSER="cedricsam@gmail.com"
+SINAPASS="LammaIsland"
+SINAAPPID="2183853018"
+#SINAUSER="YOUR_USERNAME"
+#SINAPASS="YOUR_PASSWORD"
+#SINAAPPID="YOUR_APPID"
 URL1="http://api.t.sina.com.cn/statuses/user_timeline.json?source=${SINAAPPID}&count=200"
 URL2="http://api.t.sina.com.cn/users/show.json?source=${SINAAPPID}"
 URL3="http://api.t.sina.com.cn/friends/ids.json?source=${SINAAPPID}&count=5000"
 URL4="http://api.t.sina.com.cn/followers/ids.json?source=${SINAAPPID}&count=5000"
 URL5="http://api.t.sina.com.cn/tags.json?source=${SINAAPPID}"
-#URL5="http://api.t.sina.com.cn/users/search.json?source=${SINAAPPID}&count=40" # most popular
+URL6="http://api.t.sina.com.cn/users/search.json?source=${SINAAPPID}&count=1" # most popular
 
 if [ $# -lt 3 ]
 then
@@ -23,6 +26,7 @@ case $1 in
     3)	URL=${URL3};;
     4)	URL=${URL4};;
     5)	URL=${URL5};;
+    6)	URL=${URL6};;
 esac
 
 if [ $# -gt 3 ]
@@ -42,6 +46,17 @@ case $1 in
     3) fname="friends-${fname}";;
     4) fname="followers-${fname}";;
     5) fname="tags-${fname}";;
+    6) fname="usersearch-${fname}";;
 esac
 
-curl -s -u ${SINAUSER}:${SINAPASS} "${URL}&user_id=$2" -o ${fname}
+IF=`rand -M 2`
+
+case $1 in
+    1|2|3|4|5)
+	#curl -s -u ${SINAUSER}:${SINAPASS} --interface eth${IF} "${URL}&user_id=$2" -o ${fname} ;;
+	curl -s -u ${SINAUSER}:${SINAPASS} "${URL}&user_id=$2" -o ${fname} ;;
+    6)
+	echo ${fname}
+	#curl -u ${SINAUSER}:${SINAPASS} --interface eth${IF} "${URL}&q=$2" -o ${fname} ;;
+	curl -s -u ${SINAUSER}:${SINAPASS} "${URL}&q=$2" -o ${fname} ;;
+esac
