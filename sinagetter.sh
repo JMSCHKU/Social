9861805c0934c31dfc04c8b29614919d053cc289
Cedric Sam <cedricsam@gmail.com>
2011-03-01 10:36:48 +0800

    MAXTRIES=10 MAXBLANKS=4 CONSEQ_BLANKS=0 timely update

--- sinagetter.sh
+++ sinagetter.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-USAGE="usage: sinagetter.sh [option:1=user_timeline,2=users_show,3=friends,4=followers,5=tags] [user_id] [screen_name(for saving)] [page#] "
-SINAUSER="cedricsam@gmail.com"
-SINAPASS="LammaIsland"
-SINAAPPID="2183853018"
-#SINAUSER="YOUR_USERNAME"
-#SINAPASS="YOUR_PASSWORD"
-#SINAAPPID="YOUR_APPID"
+USAGE="usage: sinagetter.sh [option:1=user_timeline,2=users_show,3=friends,4=followers,5=tags,6=mostpopularusers,7=reposts,8=comments] [user_id] [screen_name(for saving)] [page#] "
+SINAUSER="YOUR_EMAIL"
+SINAPASS="YOUR_PASSWORD"
+SINAAPPID="YOUR_APPID"
 URL1="http://api.t.sina.com.cn/statuses/user_timeline.json?source=${SINAAPPID}&count=200"
 URL2="http://api.t.sina.com.cn/users/show.json?source=${SINAAPPID}"
 URL3="http://api.t.sina.com.cn/friends/ids.json?source=${SINAAPPID}&count=5000"
 URL4="http://api.t.sina.com.cn/followers/ids.json?source=${SINAAPPID}&count=5000"
 URL5="http://api.t.sina.com.cn/tags.json?source=${SINAAPPID}"
 URL6="http://api.t.sina.com.cn/users/search.json?source=${SINAAPPID}&count=1" # most popular
+URL7="http://api.t.sina.com.cn/statuses/repost_timeline.json?source=${SINAAPPID}&count=200" # reposts
+URL8="http://api.t.sina.com.cn/statuses/comments.json?source=${SINAAPPID}&count=200" # reposts
 
 if [ $# -lt 3 ]
 then
@@ -27,13 +26,15 @@ case $1 in
     4)	URL=${URL4};;
     5)	URL=${URL5};;
     6)	URL=${URL6};;
+    7)	URL=${URL7};;
+    8)	URL=${URL8};;
 esac
 
 if [ $# -gt 3 ]
 then
     fname="$3-$4_`date +%Y%m%d-%H%M`.json"
     case $1 in
-	1)	URL="${URL}&page=$4";;
+	1|7|8)	URL="${URL}&page=$4";;
 	3|4)	URL="${URL}&cursor=$4";;
     esac
 else
@@ -47,16 +48,21 @@ case $1 in
     4) fname="followers-${fname}";;
     5) fname="tags-${fname}";;
     6) fname="usersearch-${fname}";;
+    7) fname="reposts-${fname}";;
+    8) fname="comments-${fname}";;
 esac
 
-IF=`rand -M 2`
+# For network load balancing
+D1=`date +%N`
+D2=`date +%N`
+INF="--interface eth`rand -M 2 -s ${D1}`"
+if [ `rand -M 2 -s ${D2}` -eq 0 ]; then INF=${INF}:0; fi
 
 case $1 in
     1|2|3|4|5)
-	#curl -s -u ${SINAUSER}:${SINAPASS} --interface eth${IF} "${URL}&user_id=$2" -o ${fname} ;;
-	curl -s -u ${SINAUSER}:${SINAPASS} "${URL}&user_id=$2" -o ${fname} ;;
+	curl ${INF} -s -u ${SINAUSER}:${SINAPASS} "${URL}&user_id=$2" -o ${fname} ;;
     6)
-	echo ${fname}
-	#curl -u ${SINAUSER}:${SINAPASS} --interface eth${IF} "${URL}&q=$2" -o ${fname} ;;
-	curl -s -u ${SINAUSER}:${SINAPASS} "${URL}&q=$2" -o ${fname} ;;
+	curl ${INF} -s -u ${SINAUSER}:${SINAPASS} "${URL}&q=$2" -o ${fname} ;;
+    7|8)
+	curl ${INF} -s -u ${SINAUSER}:${SINAPASS} "${URL}&id=$2" -o ${fname} ;;
 esac
--- sinapeople.sh
+++ sinapeople.sh
@@ -1,1 +1,50 @@
-/home/csam/bin/sinapeople.sh
+#!/bin/bash
+
+USAGE="usage: sinapeople.sh [1=prov,2=query] [sort=1=latest,2=nbfollowers] [page#] [prov/query] [city]"
+SINAUSER="YOUR_EMAIL"
+SINAPASS="YOUR_PASSWORD"
+SINAAPPID="YOUR_APPID"
+URL="http://api.t.sina.com.cn/users/search.json?source=${SINAAPPID}&count=40" # most popular
+
+if [ $# -lt 4 ]
+then
+    echo ${USAGE}
+    exit
+fi
+
+case $1 in
+    1)	URL="${URL}&province=$4";;
+    2)	URL="${URL}&q=$4";;
+    *)	echo ${USAGE}; exit
+esac
+
+case $2 in
+    1|2)URL="${URL}&sort=$2";;
+    *)	echo ${USAGE}; exit
+esac
+
+URL="${URL}&page=$3"
+
+fname="$4_p$3_`date +%Y%m%d-%H%M`.json"
+if [ $# -gt 4 ]
+then
+    fname="$4-$5_p$3_`date +%Y%m%d-%H%M`.json"
+    if [ $1 -eq 1 ]
+    then
+	URL="${URL}&city=$5"
+    fi
+fi
+
+case $1 in
+    1) fname="searchprov-${fname}";;
+    2) fname="search-${fname}";;
+esac
+
+echo ${URL}
+
+D1=`date +%N`
+D2=`date +%N`
+INF="--interface eth`rand -M 2 -s ${D1}`"
+if [ `rand -M 2 -s ${D2}` -eq 0 ]; then INF=${INF}:0; fi
+
+curl ${INF} -s -u ${SINAUSER}:${SINAPASS} "${URL}" -o ${fname}
