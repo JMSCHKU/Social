ab1c98d153cff9b53e7644ededef9edb44b505a1
Cedric Sam <cedricsam@gmail.com>
2012-07-30 13:14:51 +0800

    Twitter relations

--- twitter.rel.py
+++ twitter.rel.py
@@ -4,14 +4,16 @@ import sys
 import pg
 import httplib
 import simplejson
+import mypass
 
-pgconn = pg.DB('YOUR_DB', '127.0.0.1', 5432, None, None, 'YOUR_USERNAME', 'YOUR_PASSWORD')
+pgconn = mypass.getConn()
 
 usage = "twitter.rel [user_id] [--followers|--following] [OPT:--no-duplicates]"
 
 no_duplicates = False
 followers = False
 following = False
+cursor = None
 if len(sys.argv) > 1:
     user_id = sys.argv[1]
     if len(sys.argv) > 2:
@@ -22,6 +24,8 @@ if len(sys.argv) > 1:
 		followers = True
 	    elif sys.argv[i] == "--friends" or sys.argv[i] == "-1" or sys.argv[i] == "--following":
 		following = True
+	    elif (sys.argv[i] == "--cursor" or sys.argv[i] == "-c") and len(sys.argv) >= i+1:
+		cursor = sys.argv[i+1]
 else:
     print usage
     sys.exit()
@@ -54,20 +58,26 @@ if no_duplicates:
 if user_id > 0:
     conn = httplib.HTTPConnection("api.twitter.com")
     try:
-	conn.request("GET", "/1/" + pre + "/ids.json?user_id=" + user_id)
+	url = "/1/" + pre + "/ids.json?user_id=" + user_id
+	if cursor is not None:
+	    url += "&cursor=" + cursor
+	print "http://api.twitter.com/" + url
+	conn.request("GET", url)
     except (Exception):
 	sys.exit(sys.exc_info())
     r = conn.getresponse()
     sql = "INSERT INTO " + t + " (source_id, target_id, retrieved) VALUES (" + str(user_id) + ", %d, NOW()) "
     if r.status == 200:
 	j = simplejson.load(r)
-	if len(j) > 0:
-	    for x in j:
+	if len(j["ids"]) > 0:
+	    for x in j["ids"]:
 		#sel = pgconn.query(sql % x)
 		try:
 		    print pgconn.insert(t, {"source_id": user_id, "target_id": x, "retrieved": "NOW()"})
 		except pg.ProgrammingError:
 		    print "already exists: " + str(user_id) + "->" + str(x)
 		    continue
+	    if len(j["ids"]) >= 5000:
+		print j["next_cursor_str"]
     else:
 	print r.read()
